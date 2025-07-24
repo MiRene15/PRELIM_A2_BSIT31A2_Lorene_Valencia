c3fe34b4b152818be31d529e9449dfee73aa74dc
Lorene_Valencia <ladymirene@proton.me>
2025-07-24 20:27:40 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_A2_BSIT31A2_Lorene_Valencia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
